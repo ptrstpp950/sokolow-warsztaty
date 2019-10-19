cffa7deb1b4352f49a9b43f275d5f5e6a27f3aa2
Piotr Stapp <piotr.stapp@finai.com>
2019-10-19 08:56:33 +0200

    init

---
+++ js/faceDetectionControls.js
@@ -0,0 +1,105 @@
+
+const TINY_FACE_DETECTOR = 'tiny_face_detector'
+
+let selectedFaceDetector = TINY_FACE_DETECTOR
+
+
+// tiny_face_detector options
+let inputSize = 512
+let scoreThreshold = 0.5
+
+
+function getFaceDetectorOptions() {
+  return new faceapi.TinyFaceDetectorOptions({ inputSize, scoreThreshold })
+}
+
+function onIncreaseMinConfidence() {
+  minConfidence = Math.min(faceapi.round(minConfidence + 0.1), 1.0)
+  $('#minConfidence').val(minConfidence)
+  updateResults()
+}
+
+function onDecreaseMinConfidence() {
+  minConfidence = Math.max(faceapi.round(minConfidence - 0.1), 0.1)
+  $('#minConfidence').val(minConfidence)
+  updateResults()
+}
+
+function onInputSizeChanged(e) {
+  changeInputSize(e.target.value)
+  updateResults()
+}
+
+function changeInputSize(size) {
+  inputSize = parseInt(size)
+
+  const inputSizeSelect = $('#inputSize')
+  inputSizeSelect.val(inputSize)
+  inputSizeSelect.material_select()
+}
+
+function onIncreaseScoreThreshold() {
+  scoreThreshold = Math.min(faceapi.round(scoreThreshold + 0.1), 1.0)
+  $('#scoreThreshold').val(scoreThreshold)
+  updateResults()
+}
+
+function onDecreaseScoreThreshold() {
+  scoreThreshold = Math.max(faceapi.round(scoreThreshold - 0.1), 0.1)
+  $('#scoreThreshold').val(scoreThreshold)
+  updateResults()
+}
+
+function onIncreaseMinFaceSize() {
+  minFaceSize = Math.min(faceapi.round(minFaceSize + 20), 300)
+  $('#minFaceSize').val(minFaceSize)
+}
+
+function onDecreaseMinFaceSize() {
+  minFaceSize = Math.max(faceapi.round(minFaceSize - 20), 50)
+  $('#minFaceSize').val(minFaceSize)
+}
+
+function getCurrentFaceDetectionNet() {
+  if (selectedFaceDetector === TINY_FACE_DETECTOR) {
+    return faceapi.nets.tinyFaceDetector
+  }
+}
+
+function isFaceDetectionModelLoaded() {
+  return !!getCurrentFaceDetectionNet().params
+}
+
+async function changeFaceDetector(detector) {
+  selectedFaceDetector = detector
+  const faceDetectorSelect = $('#selectFaceDetector')
+  faceDetectorSelect.val(detector)
+  faceDetectorSelect.material_select()
+
+  $('#loader').show()
+  if (!isFaceDetectionModelLoaded()) {
+    await getCurrentFaceDetectionNet().load('./')
+  }
+
+  $(`#${detector}_controls`).show()
+  $('#loader').hide()
+}
+
+async function onSelectedFaceDetectorChanged(e) {
+  selectedFaceDetector = e.target.value
+
+  await changeFaceDetector(e.target.value)
+  updateResults()
+}
+
+function initFaceDetectionControls() {
+  const faceDetectorSelect = $('#selectFaceDetector')
+  faceDetectorSelect.val(selectedFaceDetector)
+  faceDetectorSelect.on('change', onSelectedFaceDetectorChanged)
+  faceDetectorSelect.material_select()
+
+  const inputSizeSelect = $('#inputSize')
+  inputSizeSelect.val(inputSize)
+  inputSizeSelect.on('change', onInputSizeChanged)
+  inputSizeSelect.material_select()
+}
